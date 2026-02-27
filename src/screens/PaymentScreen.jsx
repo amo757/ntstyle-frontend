8b832e3c9bc629cdd62401c4f43ac4dfa0ecc10d
amo757 <81531720+amo757@users.noreply.github.com>
2026-02-27 16:45:18 +0400

    first commit

--- src/screens/PaymentScreen.jsx
+++ src/screens/PaymentScreen.jsx
@@ -208,7 +208,8 @@ const PaymentScreen = () => {
                                     <span className="text-[11px] text-gray-500">Secure payment via TBC Bank gateway</span>
                                 </div>
                                 <div className="ml-auto flex gap-2">
-                                    <img src="/visa.png" alt="Visa-Mastercard" className="h-6 object-contain" />
+                                    <img src="https://upload.wikimedia.org/wikipedia/commons/0/04/Visa.svg" alt="Visa" className="h-4" />
+                                    <img src="https://upload.wikimedia.org/wikipedia/commons/2/2a/Mastercard-logo.svg" alt="Mastercard" className="h-4" />
                                 </div>
                             </div>
                             {paymentMethod === 'tbc' && (
